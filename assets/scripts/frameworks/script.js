854be348ca4602cbcf7547f2bf292ac07107a087
Wallace Santana <wallacesantanak0@gmail.com>
2024-10-13 11:38:56 -0300

    Adicionando Api para reviews

--- assets/scripts/frameworks/script.js
+++ assets/scripts/frameworks/script.js
@@ -1,13 +1,73 @@
-let  swiper = new Swiper(".mySwiper", {
+//Swiper Slide
+let swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
+        delay: 2000,
+        disableOnInteraction: false,
     },
- 
+
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-}) 
+});
+
+let  swiperReviews = new Swiper('.swiper-container-reviews', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// const swiperAvaliacoes = new Swiper('.swiper-container-reviews', {
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true,
+//         dynamicMainBullets: 2,
+//     },
+//     breakpoints: {
+//         640: {
+//             slidesPerView: 1,
+//             spaceBetween: 10,
+//         },
+//         768: {
+//             slidesPerView: 2,
+//             spaceBetween: 5,
+//         },
+//         1024: {
+//             slidesPerView: 2,
+//             spaceBetween: 10,
+//         },
+//     },
+//     // autoplay: {
+//     //     delay: 2000,
+//     // },
+// });
+
+AOS.init();
+AOS.init({
+    disable: false,
+    startEvent: "DOMContentLoaded",
+    initClassName: "aos-init",
+    animatedClassName: "aos-animate",
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    offset: 120,
+    delay: 0,
+    duration: 400,
+    easing: "ease",
+    once: false,
+    mirror: false,
+    anchorPlacement: "top-bottom",
+});
--- assets/scripts/srcipts.js
+++ assets/scripts/srcipts.js
@@ -1,10 +1,49 @@
-const header = document.querySelector('header');
-const msgContainer = document.getElementsByClassName('.msg-container');
-window.addEventListener('scroll', () => {
-    
+const header = document.querySelector("header");
+const msgContainer = document.getElementsByClassName(".msg-container");
+window.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
-        header.classList.add('scrolled'); 
+        header.classList.add("scrolled");
     } else {
-        header.classList.remove('scrolled'); 
+        header.classList.remove("scrolled");
     }
 });
+
+const placeId = "ChIJ_b0SMKTQmwARPTj2a-uC4So";
+const apiKey = "AIzaSyCrFeev14_VsyACy7vpbeSd_20VHrk5PWk";
+
+async function fetchPlaceDetails() {
+    try {
+        const response = await fetch(
+            `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJ_b0SMKTQmwARPTj2a-uC4So&key=AIzaSyCrFeev14_VsyACy7vpbeSd_20VHrk5PWk`
+        );
+        const data = await response.json();
+
+        if (data && data.result) {
+            const reviews = data.result.reviews;
+            reviews.forEach((review) => {
+                populateReviewCard(review);
+            });
+        }
+    } catch (error) {
+        console.error("Erro ao buscar os detalhes do local:", error);
+    }
+}
+
+
+function populateReviewCard(review) {
+    document.querySelector(".review-text").innerHTML = `"${review.text}"`;
+    document.querySelector(".review-client-name").innerHTML = `${review.author_name}`;
+
+    if (review.profile_photo_url) {
+        document.querySelector(".client-img").src = review.profile_photo_url;
+    }
+    const starCount = review.rating;
+    let starHtml = "";
+    for (let i = 0; i < starCount; i++) {
+        starHtml += "⭐";
+    }
+    document.querySelector(".review-stars").innerHTML = starHtml;
+    document.querySelector(".swiper-wrapper").appendChild(reviewCardTemplate);
+}
+
+document.addEventListener("DOMContentLoaded", fetchPlaceDetails);
